08f6d77c49c2d628e0099e88eeadef382a53b494
Denis <109104246+Denis0014@users.noreply.github.com>
2024-09-03 10:34:52 +0300

    Update log_muncher.rb

--- task/log_muncher.rb
+++ task/log_muncher.rb
@@ -1,2 +1,18 @@
+# TODO
+# 1. Парсинг
+# └ |   f = File.open(fname)
+#   |   f.read_line
+# 2. Тест на ограниченном наборе
+# 3. Расстояние Левенштейна
+
 include Gem::Text
-# levenshtein_distance('asd', 'sdf') # => 2
+
+    def parse(fname)
+        
+    end
+
+    def levenshtein_distance(a, b):
+
+    end
+
+ #  levenshtein_distance('asd', 'sdf') # => 2
